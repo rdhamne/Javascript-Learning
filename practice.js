cad14280d0c59f4c26da9ec54368232b0a0d5d9a
Rohit DHAMNE <rdhamne@c02d459fml7h.ad.mdsol.com>
2022-11-23 14:10:08 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+const mobile = {
+    model : 'realme',
+    ram : 12,
+    rom : 256,
+    display : {
+        type : 'super amoled',
+        resolution : '1080p'
+    },
+    processor : 'dimensity 1200',
+    mode : 'off',
+    power : function (md){
+        this.mode = md;
+    }
+};
+
+class newMobile {
+
+}
