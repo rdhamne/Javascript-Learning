53e55a8bcfd1db537726cb1d5e4a2f39a82e698f
Rohit <rdhamne@mdsol.com>
2022-11-25 09:14:10 +0530

    commmit

--- practice.js
+++ practice.js
@@ -15,16 +15,32 @@
 
 // class
 
-class Car {
-  constructor(name, colour, year) {
+// class Car {
+//   constructor(name, colour, year) {
+//     this.name = name;
+//     this.colour = colour;
+//     this.year = year;
+//   }
+// }
+
+// let car1 = new Car("alto", "white", 2001);
+// console.log(car1["name"]);
+
+// let car2 = new Car("swift", "white", 2016);
+// console.log(car2["colour"]);
+
+//book challenge
+
+class Book {
+  constructor(name, author, pages) {
     this.name = name;
-    this.colour = colour;
-    this.year = year;
+    this.author = author;
+    this.pages = pages;
   }
 }
 
-let car1 = new Car("alto", "white", 2001);
-console.log(car1["name"]);
-
-let car2 = new Car("swift", "white", 2016);
-console.log(car2["colour"]);
+let book1 = new Book("Think Like a Monk", "Jay shetty", 326);
+let book2 = new Book("Automic Habits", "James Clear", 256);
+let book3 = new Book("Alchemist", "Paulo Coehlo", 320);
+let book4 = new Book("The Institute", "Stephen King", 572);
+let book5 = new Book("Mad Honey", "Jennifer", 230);
